0151adb8e0d16844d698725170233c88d369ce2d
Kevin Alfonso <kevin.vm1511@gmail.com>
2024-01-30 16:21:09 -0600

    rhodep: Bootanimation

--- rom/pixys.mk
+++ rom/pixys.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/motorola/targets/include/kernel/source.mk)
 # Libhidl
 PLATFORM_PROVIDES_LIBHIDL := true
 
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Required Scripts
 $(warning This ROM requires replace_camera_sepolicy.sh to be ran...)
 
